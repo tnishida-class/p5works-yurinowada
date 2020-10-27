677b40b66848491c6378c678104b52151da9b55b
yurinowada <yurino.wada1115@gmail.com>
2020-10-27 13:23:02 +0900

    section2-q1
    yurinowada,1870873H

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let r = i * 10 + 10;
+    if(i < 5){
+      stroke(0, 0, 255);
+    }
+    else{
+      stroke(255, 0, 0);
+    }
+    ellipse(50, 50, r, r);
   }
 }
